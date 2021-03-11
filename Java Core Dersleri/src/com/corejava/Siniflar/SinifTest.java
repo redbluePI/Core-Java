b126ff599e09228650eef9a8de6ad3bb2d109923
Acer <Acer@LAPTOP-63VA5FEB>
2021-03-11 10:47:43 +0100

    SinifTest.java ve Soyutlama.java

---
+++ Java Core Dersleri/src/com/corejava/Siniflar/SinifTest.java
@@ -0,0 +1,50 @@
+package com.corejava.Siniflar;
+
+public class SinifTest {
+	String test1 = "basarili";
+	public static void main(String[] args) {
+
+		int i = 4;
+		int k = i;
+		i = 5;
+		k = i;
+		System.out.println(k);
+		
+		System.out.println("----------------------------------------------------");
+//default degerler
+		Kullanici k1 = new Kullanici();
+		System.out.println("Varsayilan degerler:" + k1.isim + " " + k1.id);
+		
+		k1.isim = "Ahmet";
+		k1.id = 323;
+		System.out.println(k1.isim + " " + k1.id);
+		
+		Kullanici k2;
+		k2 = k1;
+		k1.id = 209;
+		System.out.println(k2.isim + " " + k2.id);
+		//k2 = new Kullanici();
+		System.out.println(k2.isim + " " + k2.id);
+		
+		System.out.println(k1.isimYazdir());
+		System.out.println("----------------------------------------------------");
+		SinifTest STest = new SinifTest();
+		System.out.println(STest.test1);
+	}
+
+}
+class Kullanici{
+	String isim;
+	int id;
+	boolean b;
+	char c;
+	double d;
+	
+//metotlar
+	void durumVer() {
+		System.out.println("Isim: " + isim + " ID: " + id);
+	}
+	String isimYazdir() {
+		return "Isim: " + isim;
+	}
+}
---
+++ Java Core Dersleri/src/com/corejava/Siniflar/SoyutlamaTest.java
@@ -0,0 +1,37 @@
+package com.corejava.Siniflar;
+
+public class SoyutlamaTest {
+
+	public static void main(String[] args) {
+		Ogrenci o1 = new Ogrenci();
+		o1.isim = "Ahmet";
+		o1.no = 729;
+		o1.k1 = new Kalem();
+		o1.k1.renk = "mavi";
+		o1.k1.tip = 1;
+		
+		o1.yaz();
+		Ogrenci o2 = new Ogrenci();
+		o2.isim = "Mehmet";
+		o2.no = 735;
+		o2.k1 = new Kalem();
+		o2.k1.renk = "siyah";
+		o2.k1.tip = 1;
+		o2.yaz();
+	}
+
+}
+class Kalem{
+	String renk;
+	int tip;
+}
+class Ogrenci{
+	String isim;
+	int no;
+	Kalem k1;
+	
+	void yaz() {
+		System.out.println("Ögrenci " + isim + ", " + k1.renk + " ile yaziyor.....");
+	}
+	
+}

7a405e9fd7d06ed585b3e0d381c85def7c54d305
Acer <Acer@LAPTOP-63VA5FEB>
2021-03-13 12:56:32 +0100

    Miras.java

--- Java Core Dersleri/src/com/corejava/Siniflar/MirasTest.java
+++ Java Core Dersleri/src/com/corejava/Siniflar/MirasTest.java
@@ -4,19 +4,55 @@ public class MirasTest {
 
 	public static void main(String[] args) {
 		SubClass SC = new SubClass();
-		SC.yaz();
-
+		SC.getInfo();
+		System.out.println("---------------------------------------------------------------");
+//super ve override		
+		Kivi k1 = new Kivi("Kivi 1");
+		k1.yenebilir();
+		System.out.print(k1);
 	}
 
 }
+class Meyve{
+	String isim;
+	public Meyve(String isim) {
+		this.isim = isim;
+		System.out.println("Meyve çağrıldı...");
+	}
+	protected void yenebilir(){
+		System.out.println("Bu meyve yenebilir...");
+	}
+}
+class Kivi extends Meyve{
 
+	public Kivi(String isim) {
+		super(isim);
+		super.yenebilir();
+	}
+	
+	@Override
+	protected void yenebilir() {
+		System.out.println("Bu Kivi yenebilir...");
+	}
+	
+	@Override
+	public String toString() {
+		return "Bu meyvenin ismi: " + isim;
+	}
+}
+class Çilek extends Meyve{
 
+	public Çilek(String isim) {
+		super(isim);
+	}
+	
+}
 class SuperClass{
 	protected double d = 3.1415;
 	public SuperClass() {
 		// TODO Auto-generated constructor stub
 	}
-	void yaz() {
+	void getInfo() {
 		System.out.println(d);
 	}
 }

4f630a8af5f5ee5284df81f053a2670e973fe48e
Acer <Acer@LAPTOP-63VA5FEB>
2021-03-15 16:25:56 +0100

    HashMap

--- Java Core Dersleri/src/com/corejava/Koleksiyonlar/BagliNesneler2.java
+++ Java Core Dersleri/src/com/corejava/Koleksiyonlar/BagliNesneler2.java
@@ -12,7 +12,24 @@ public class BagliNesneler2 {
 		Collections.sort(sokak, Comparator.comparing(Bina::getNo));
 		
 		System.out.println(sokak);
-		
+		System.out.println("...............................................................................");
+		HashMap<Integer, String> h1 = new HashMap<Integer, String>();
+		h1.put(34, "Istanbul");
+		h1.put(06, "Ankara");
+		h1.put(16, "Bursa");
+		h1.put(58, "Sivas");
+		h1.put(16, "Bursa");
+		System.out.println(h1);
+		h1.remove(16);
+		System.out.println(h1);
+		for(Map.Entry<Integer, String> e : h1.entrySet()) {
+			System.out.println(e.getValue());
+		}
+		System.out.println("...............................................................................");
+		HashMap<Integer, Bina> binalar = new HashMap<Integer, Bina>();
+		binalar.put(321, new Bina("Yıldız", 321));
+		binalar.put(541, new Bina("Yıldız", 541));
+		System.out.println(binalar);
 	}
 
 }
